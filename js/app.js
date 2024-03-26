4a4951d9b0a3125792c243f885c386211ecdf572
Simone0730 <simone0730@hotmail.com>
2024-03-26 18:05:00 +0100

    Inserimento commenti

--- js/app.js
+++ js/app.js
@@ -8,18 +8,25 @@ console.log (km);
 const eta = prompt ('Quanti anni hai?')
 console.log (eta);
 
+//Calcolare il prezzo totale
 const prezzoTot = (0.21 * km) 
-console.log (prezzoTot);
 
+//Calcolare gli sconti
 const scontoUnder = (prezzoTot * 20 / 100)
 const scontoOver = (prezzoTot * 40 / 100)
+const prezzoUnder = (prezzoTot - scontoUnder)
+const prezzoOver = (prezzoTot - scontoOver)
 
+//SE si ha meno di 18 anni si avrà uno sconto del 20%
 if(eta < 18){
-    console.log(prezzoTot - scontoUnder);
+    console.log('Il prezzo del biglietto è', prezzoUnder.toFixed(2));
 }
+//ALTRIMENTI SE si ha più di 65 anni si avrà uno sconto del 40%
 else if(eta > 65){
-    console.log(prezzoTot - scontoOver)
+    console.log('Il prezzo del biglietto è', prezzoOver.toFixed(2));
  }
+ //ALTRIMENTI il prezzo rimarrà quello totale
  else{
-    console.log(prezzoTot);
+    console.log('Il prezzo del biglietto è', prezzoTot);
  }
+
